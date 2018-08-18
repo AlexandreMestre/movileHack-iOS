f05305661ba061600b8e168d87645780a0a51d14
Alexandre Mestre <alexandre.conti.mestre@gmail.com>
2018-08-18 17:20:04 -0300

    Code documentation

--- MarketLeftOver/MarketLeftOver/Business/ServicesProtocols/MarketServiceProtocol.swift
+++ MarketLeftOver/MarketLeftOver/Business/ServicesProtocols/MarketServiceProtocol.swift
@@ -11,9 +11,22 @@ import CoreLocation
 
 protocol MarketServiceProtocol: class {
 
+    /// Get the market with id provided
+    ///
+    /// - Parameter id: market id
+    /// - Returns: Market object if exists
     func market(withId id: String) -> Market?
+    
+    /// Get markets near a location
+    ///
+    /// - Parameter location: given location
+    /// - Returns: Array with markets nearby
     func markets(near location: CLLocation) -> [Market]
     
+    /// Get product categories in a market
+    ///
+    /// - Parameter market: market object
+    /// - Returns: all categories in given market
     func categories(of market: Market) -> [Category]
     
 }
