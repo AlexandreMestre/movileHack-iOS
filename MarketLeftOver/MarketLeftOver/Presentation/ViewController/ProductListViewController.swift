549b46268bf253cb78c6becf185c3df2be0d3954
Alexandre Mestre <alexandre.conti.mestre@gmail.com>
2018-08-18 18:37:04 -0300

    Product list view

---
+++ MarketLeftOver/MarketLeftOver/Presentation/ViewController/ProductListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ProductListViewController.swift
+//  MarketLeftOver
+//
+//  Created by Alexandre Conti Mestre on 18/08/18.
+//  Copyright © 2018 Alexandre Conti Mestre. All rights reserved.
+//
+
+import UIKit
+
+class ProductListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension ProductListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
